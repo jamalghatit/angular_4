11c66dc9ad57c58209c8b65b0316c56d4d688e89
Jamal Andreoti Ghatit <jamalghatit@gmail.com>
2022-01-10 20:45:24 -0300

    reusable apps

--- Section_5_Building_Re-usable_component/section5/src/app/favorite/favorite.component.ts
+++ Section_5_Building_Re-usable_component/section5/src/app/favorite/favorite.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'favorite',
   templateUrl: './favorite.component.html',
-  styleUrls: ['./favorite.component.css']
+  styleUrls: ['./favorite.component.css'],
+  inputs: ['isFavorite' ]
 })
 
-export class FavoriteComponent{
+export class FavoriteComponent implements OnInit{
+  @Input('is-favorite') isFavorite: boolean = false;
+  @Output() change = new EventEmitter();
 
-  isFavorite:boolean = true;
+  constructor(){};
+
+  ngOnInit() {};
+
   onClick(){
     this.isFavorite = !this.isFavorite;
+    this.change.emit();
+    /*
+    we have a method called emit, and we use that to raise or
+    publish an event, which basically means notifying others
+    that something has happened.
+    */
   }
 }
